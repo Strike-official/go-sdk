cb41eba1ed03e318dfe110f99d43caada6ac64f5
sayak9495 <sayak1997sen@gmail.com>
2021-09-09 02:34:18 +0530

    Added Text-Input and Text(question) SDKs

---
+++ strike/textInput_test.go
@@ -0,0 +1,22 @@
+package strike_test
+
+import (
+	"fmt"
+	"testing"
+
+	strike "github.com/strike-official/go-sdk/strike"
+)
+
+func TestInputText(t *testing.T) {
+
+	strikeObj := strike.Create("location_handler", "https://abc.com") //This creates the bare minimum of the response
+
+	question1 := strikeObj.Question("Key1").
+		QuestionText().
+		SetTextToQuestion("How are you?", "Text Description, getting used for testing purpose.")
+
+	question1.Answer(true).
+		TextInput("Input Description")
+
+	fmt.Println(string(strikeObj.ToJson())) //ToJson() just converts the struct to json value
+}
